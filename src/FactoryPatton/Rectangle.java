c179da064c9da179dbd635c1db5a57f0374b48a7
Azura <azura@126.com>
2017-02-02 11:03:46 +0800

    create the java design pattern project

---
+++ src/FactoryPatton/Rectangle.java
@@ -0,0 +1,8 @@
+package FactoryPatton;
+
+public class Rectangle implements Shape{
+	 @Override
+	   public void draw(){
+	      System.out.println("Inside Rectangle::draw() method.");
+	 }
+}
---
+++ src/FactoryPatton/Shape.java
@@ -0,0 +1,6 @@
+package FactoryPatton;
+
+public interface Shape {
+
+	void draw();
+}
